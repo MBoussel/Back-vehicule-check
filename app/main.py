92b9d8704db8b47ad8277f3065c5ed2c7a761a5c
MBoussel <mb.boussel@gmail.com>
2026-04-24 17:15:55 +0200

    Fix CORS for Netlify domain

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://ton-front.com",
+    "https://checkthecar.netlify.app",
 ]
 
 app.add_middleware(
